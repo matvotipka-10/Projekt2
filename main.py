705d41849c1aac7bd85849d30ba7706b4cc604b1
matvotipka-10 <matvotipka@gmail.com>
2025-11-23 14:21:37 +0100

    funkce compare_number

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Enter a number:
 -----------------------------------------------
 """)
 
-def random_number() -> int:
+def random_number() -> str:
     """
     Funkce vrací náhodné čtyřmístné číslo.
     Nesmí začínat 0 a číslice se nejsmějí opakovat.
@@ -23,7 +23,8 @@ def random_number() -> int:
         for n in list_number:
             if n != 0 and n not in random_list_number:
                 random_list_number[0] = n
-    random_number = int(''.join(map(str, random_list_number)))
+    random_number = (''.join(map(str, random_list_number)))
+    print(random_number)
     return random_number
     
 def player_guess() -> str:
@@ -31,9 +32,9 @@ def player_guess() -> str:
     Hráč zadává svůj tip na čtyřmístné číslo.
     """
     player_number = (input(">>> "))        
-    return player_number
+    return (player_number)
 
-def control_player_number(player_number: str) -> bool:
+def control_player_number(player_number: str) -> str:
     """
     Funkce přijme string. Zkontroluje postupně: délka je 4, obsahuje jen číslice,
     nezačíná "0", nemá duplicitní znaky. Pokud je vše splněno vrátí True, jinak vždy vrátí False.
@@ -47,33 +48,37 @@ def control_player_number(player_number: str) -> bool:
     if player_number [0] == "0":
         print("Your number starts with 0.")
         return False
-    if not len(player_number) == len(set(player_number)):
+    if not len(player_number) == len(set(str(player_number))):
         print("There are duplicates in your number.")
-        return False
+        return None
     else:
-        return True
+        return player_number
  
+def compare_numbers(player_number: str, random_number: str) -> tuple:
+    """
+    Funkce porovnává číslice z player_number a random_number. Pokud je číslice stejná
+    a zároveň na stejné pozici, znamená to 1 bull. Pokud se číslice jen nachází,
+    ale není na stejné pozici jedná se o cow.
+    random_number = 2017
+    player number = 2107
+    bulls = 2
+    cows = 2
+    """
+    bulls = 0
+    cows = 0
+    for i in range(len(player_number)):
+        if player_number[i] == random_number[i]:
+            bulls += 1  
+    for n in range(len(player_number)):
+        if random_number[n] != random_number[n]:  
+            if player_number(n) in random_number:
+                cows += 1
+    print(bulls)
+    print(cows)
 
-                    
+    return bulls, cows
     
 
 
-
-
-
-
-#def control_numbers(random_number: int, guess_number: int) -> bool:
-#    if 
-
-
-
-
-
-
-
-
-
-
 greeting()
-random_number()
-player_guess()
+compare_numbers(control_player_number(player_guess()), random_number())
