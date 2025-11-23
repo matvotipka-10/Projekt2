9951ee63a13264b34a876ab03656c9d4fe1ed78b
matvotipka-10 <matvotipka@gmail.com>
2025-11-23 16:27:17 +0100

    poslední úpravy, počítadlo času -> komplet

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
+import time
 
 def greeting():
-    """Funkce pozdraví hráče a získá od něj čtyřmístné číslo."""
+    """Funkce pozdraví hráče."""
     print("""
 Hi there!
 -----------------------------------------------
@@ -9,12 +10,11 @@ I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.
 -----------------------------------------------
 Enter a number:
------------------------------------------------
-""")
+-----------------------------------------------""")
 
 def random_number() -> str:
     """
-    Funkce vrací náhodné čtyřmístné číslo.
+    Funkce vrací string náhodného čtyřmístného čísla.
     Nesmí začínat 0 a číslice se nejsmějí opakovat.
     """
     list_number = list(range(0,10))
@@ -24,35 +24,38 @@ def random_number() -> str:
             if n != 0 and n not in random_list_number:
                 random_list_number[0] = n
     random_number = (''.join(map(str, random_list_number)))
-    print(random_number)
     return random_number
     
 def player_guess() -> str:
     """
-    Hráč zadává svůj tip na čtyřmístné číslo.
+    Funcke vrací hráčův tip na čtyřmístné číslo.
     """
     player_number = (input(">>> "))        
     return (player_number)
 
-def control_player_number(player_number: str) -> str:
+def control_player_number(player_number: str) -> bool:
     """
-    Funkce přijme string. Zkontroluje postupně: délka je 4, obsahuje jen číslice,
-    nezačíná "0", nemá duplicitní znaky. Pokud je vše splněno vrátí True, jinak vždy vrátí False.
+    Funkce přijme string (hráčův tip). Zkontroluje postupně: délka je 4, obsahuje jen číslice,
+    nezačíná "0", nemá duplicitní znaky. Pokud je vše splněno vrátí True, jinak vrátí False.
     """
     if len(player_number) != 4:
-        print("You did not enter a 4 digit number.")
+        print("""You did not enter a 4 digit number.
+-----------------------------------------------""")
         return False
     if not player_number.isdigit():
-        print("You did not enter a number.")
+        print("""You did not enter a number.
+-----------------------------------------------""")
         return False
     if player_number [0] == "0":
-        print("Your number starts with 0.")
+        print("""Your number starts with 0.
+-----------------------------------------------""")
         return False
-    if not len(player_number) == len(set(str(player_number))):
-        print("There are duplicates in your number.")
-        return None
+    if not len(player_number) == len(set(player_number)):
+        print("""There are duplicates in your number.
+-----------------------------------------------""")
+        return False
     else:
-        return player_number
+        return True
  
 def compare_numbers(player_number: str, random_number: str) -> tuple:
     """
@@ -69,16 +72,43 @@ def compare_numbers(player_number: str, random_number: str) -> tuple:
     for i in range(len(player_number)):
         if player_number[i] == random_number[i]:
             bulls += 1  
-    for n in range(len(player_number)):
-        if random_number[n] != random_number[n]:  
-            if player_number(n) in random_number:
-                cows += 1
-    print(bulls)
-    print(cows)
-
-    return bulls, cows
+        else:    
+            if player_number[i] in random_number:
+                    cows += 1
     
+    if bulls <= 1 and cows <= 1:
+        print(f"""{bulls} bull, {cows} cow
+-----------------------------------------------""")
+    elif bulls >= 2 and cows <= 1:
+        print(f"""{bulls} bulls, {cows} cow
+-----------------------------------------------""")
+    elif bulls <= 1 and cows >= 2:
+        print(f"""{bulls} bull, {cows} cows
+-----------------------------------------------""")
+    else:
+        print(f"""{bulls} bulls, {cows} cows
+-----------------------------------------------""")    
+    
+    return bulls
+    
+if __name__ == "__main__":
+    greeting()
+    x = random_number()
+    guesses = 0
+    start_time = time.time
+    while True:
+        player = player_guess()
+        guesses += 1
+        if control_player_number(player) is True:
+            if compare_numbers(player, x) == 4:
+                end_time = time.time
+                duration = end_time - start_time
+                print(f"""Correct, you've guessed the right number
+in {guesses} guesses.
+Time: {duration:.2f} seconds.
+-----------------------------------------------
+That's amazing!""")
+                break
 
-
-greeting()
-compare_numbers(control_player_number(player_guess()), random_number())
+                
+        
